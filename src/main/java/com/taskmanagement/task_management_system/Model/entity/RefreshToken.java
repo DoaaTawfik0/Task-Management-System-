d60e245a3561b9625e3ffeea27efbf1421c915ec
Hasnaa <a02040456@gmail.com>
2026-04-23 17:38:08 +0200

    add explicit @JoinColumn to RefreshToken user association

--- src/main/java/com/taskmanagement/task_management_system/Model/entity/RefreshToken.java
+++ src/main/java/com/taskmanagement/task_management_system/Model/entity/RefreshToken.java
@@ -4,6 +4,7 @@ package com.taskmanagement.task_management_system.Model.entity;
 import com.taskmanagement.task_management_system.Base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,6 +25,7 @@ public class RefreshToken extends BaseEntity<Long> {
     private String token;
 
     @OneToOne
+    @JoinColumn(name = "user_id")
     private Users user;
 
     private LocalDateTime expiryDate;
