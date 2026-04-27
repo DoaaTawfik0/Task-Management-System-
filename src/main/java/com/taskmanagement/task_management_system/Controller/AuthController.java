b03ab131664b4836c2b603ba63f0be1f697aeb9b
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2026-04-27 18:56:12 +0300

    modifying name of controller to 'AuthController'

--- src/main/java/com/taskmanagement/task_management_system/Controller/AuthController.java
+++ src/main/java/com/taskmanagement/task_management_system/Controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-public class UserController {
+public class AuthController {
 
     private final UserService service;
 
