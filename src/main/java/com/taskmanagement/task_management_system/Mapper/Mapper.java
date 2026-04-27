1545a2839344160d19a1491a5887d8548b8b7431
Hasnaa <a02040456@gmail.com>
2026-04-27 20:32:04 +0300

    module mapper && team mapper

--- src/main/java/com/taskmanagement/task_management_system/Mapper/Mapper.java
+++ src/main/java/com/taskmanagement/task_management_system/Mapper/Mapper.java
@@ -1,7 +1,10 @@
 package com.taskmanagement.task_management_system.Mapper;
 
+import java.util.List;
+
 public interface Mapper <A , B>{
     // a => entity , b => dto
     B mapTo(A a);
     A mapFrom(B b);
+    List<B> mapToList(List<A> a);
 }
--- src/main/java/com/taskmanagement/task_management_system/Mapper/TeamMapper.java
+++ src/main/java/com/taskmanagement/task_management_system/Mapper/TeamMapper.java
@@ -1,10 +1,14 @@
 package com.taskmanagement.task_management_system.Mapper;
 
 import com.taskmanagement.task_management_system.Model.dto.TeamResponseDto;
+import com.taskmanagement.task_management_system.Model.dto.UpdateTeamRequestDto;
 import com.taskmanagement.task_management_system.Model.entity.Team;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -18,5 +22,19 @@ public class TeamMapper implements Mapper<Team , TeamResponseDto> {
     @Override
     public Team mapFrom(TeamResponseDto teamResponseDto) {
         return mapper.map(teamResponseDto , Team.class);
+    }
+
+    @Override
+    public List<TeamResponseDto> mapToList(List<Team> a) {
+        return a.stream().map(this::mapTo).collect(Collectors.toList());
+
+    }
+
+    public void updateTeam(UpdateTeamRequestDto requestDto , Team team) {
+            if(requestDto != null) {
+                if(requestDto.getName() != null) team.setName(requestDto.getName());
+                if(requestDto.getDescription() != null) team.setDescription(requestDto.getDescription());
+
+            }
     }
 }
