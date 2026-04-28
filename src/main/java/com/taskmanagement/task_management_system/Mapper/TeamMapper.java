2ae906ee702827791389e84098b43c2bffe955eb
Hasnaa <a02040456@gmail.com>
2026-04-28 07:58:05 +0300

    team mapper

--- src/main/java/com/taskmanagement/task_management_system/Mapper/TeamMapper.java
+++ src/main/java/com/taskmanagement/task_management_system/Mapper/TeamMapper.java
@@ -1,6 +1,7 @@
 package com.taskmanagement.task_management_system.Mapper;
 
 
+import com.taskmanagement.task_management_system.Model.dto.TeamInfo;
 import com.taskmanagement.task_management_system.Model.dto.TeamRequest;
 import com.taskmanagement.task_management_system.Model.dto.UpdateTeamRequest;
 import com.taskmanagement.task_management_system.Model.entity.Team;
@@ -12,8 +13,8 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TeamMapper {
     Team toEntity(TeamRequest dto);
-    TeamRequest toDto(Team entity);
-    List<TeamRequest> toDtos(List<Team> requests);
+    TeamInfo toDto(Team entity);
+    List<TeamInfo> toDtos(List<Team> requests);
     void updateTeamFromDto(UpdateTeamRequest dto, @MappingTarget Team entity);
 
 }
