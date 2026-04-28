93999ae57b5f3a1d7446f64edda43ffd649b2fd4
Hasnaa <a02040456@gmail.com>
2026-04-28 07:58:48 +0300

    team dtos

---
+++ src/main/java/com/taskmanagement/task_management_system/Model/dto/TeamRequest.java
@@ -0,0 +1,19 @@
+package com.taskmanagement.task_management_system.Model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamRequest {
+
+    private String name;
+
+    private String description;
+
+
+}
