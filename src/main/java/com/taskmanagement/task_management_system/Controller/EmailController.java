bd5b7455dc7855e654771acf4418414c0a71aa9c
Hasnaa <a02040456@gmail.com>
2026-05-01 18:17:02 +0300

    add: mail controller && send mail endpoint

---
+++ src/main/java/com/taskmanagement/task_management_system/Controller/EmailController.java
@@ -0,0 +1,25 @@
+package com.taskmanagement.task_management_system.Controller;
+
+import com.taskmanagement.task_management_system.Service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+    private final EmailService service;
+
+    @PostMapping("/send")
+    public String sendEmail
+            (@RequestParam String to,
+             @RequestParam String subject,
+             @RequestParam String text) {
+        service.sendSimpleMessage(to , subject , text);
+        return "Email sent successfully!";
+
+    }
+}
