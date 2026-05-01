a2fcfcfef03afce7e1272f9b832d83a96c3062f1
Hasnaa <a02040456@gmail.com>
2026-05-01 19:11:50 +0300

    add: email template service

--- src/main/java/com/taskmanagement/task_management_system/Service/EmailService.java
+++ src/main/java/com/taskmanagement/task_management_system/Service/EmailService.java
@@ -1,21 +1,25 @@
 package com.taskmanagement.task_management_system.Service;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
-
+    private final TemplateEngine templateEngine;
     @Value("${spring.mail.username}")
     private String fromEmail;
 
     public void sendSimpleMessage(String to, String subject, String text) {
-
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(fromEmail);
         message.setTo(to);
@@ -23,5 +27,24 @@ public class EmailService {
         message.setText(text);
         mailSender.send(message);
 
+    }
+
+    public void sendTemplateMessage(String to, String name) {
+        Context context = new Context();
+        context.setVariable("name", name);
+
+        String processHtml = templateEngine.process("email-template", context);
+        MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMailMessage, "utf-8");
+
+        try {
+            helper.setText(processHtml, true);
+            helper.setTo(to);
+            helper.setSubject("Welcome to Our Service!");
+            helper.setFrom(fromEmail);
+            mailSender.send(mimeMailMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
