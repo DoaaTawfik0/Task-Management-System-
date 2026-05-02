92ff0a35631f269236a7b64703b45c4d5de0e78d
Hasnaa <a02040456@gmail.com>
2026-05-02 16:16:59 +0300

    add: notifications endpoints

---
+++ src/main/java/com/taskmanagement/task_management_system/Controller/NotificationController.java
@@ -0,0 +1,34 @@
+package com.taskmanagement.task_management_system.Controller;
+
+import com.taskmanagement.task_management_system.Model.dto.notification.NotificationRequest;
+import com.taskmanagement.task_management_system.Model.dto.notification.NotificationResponse;
+import com.taskmanagement.task_management_system.Service.notification.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+    private final NotificationService service;
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponse>> get() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> send(@RequestBody NotificationRequest request) {
+        service.sendNotification(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PatchMapping("/{id}/read")
+    public ResponseEntity<Void> read(@PathVariable Long id) {
+        service.markAsRead(id);
+        return ResponseEntity.noContent().build();
+    }
+}
