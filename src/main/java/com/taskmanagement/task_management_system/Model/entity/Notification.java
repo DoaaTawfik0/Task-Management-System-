fedd095d0ef22c2ddc7109d35249b8e8b7eebe1b
Hasnaa <a02040456@gmail.com>
2026-05-02 14:40:06 +0300

    add: notification entity

---
+++ src/main/java/com/taskmanagement/task_management_system/Model/entity/Notification.java
@@ -0,0 +1,34 @@
+package com.taskmanagement.task_management_system.Model.entity;
+
+import com.taskmanagement.task_management_system.Base.BaseEntity;
+import com.taskmanagement.task_management_system.Enum.NotificationStatus;
+import com.taskmanagement.task_management_system.Enum.NotificationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseEntity<Long> {
+
+    private String content;
+
+    private String recipient;
+
+    private String subject;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus notificationStatus;
+}
