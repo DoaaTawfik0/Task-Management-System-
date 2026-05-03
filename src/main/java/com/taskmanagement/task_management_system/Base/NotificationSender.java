b420bf324483aa410227c7cada2ce981f5394c04
Hasnaa <a02040456@gmail.com>
2026-05-03 19:15:29 +0300

    use notification request dto instead of entity

--- src/main/java/com/taskmanagement/task_management_system/Base/NotificationSender.java
+++ src/main/java/com/taskmanagement/task_management_system/Base/NotificationSender.java
@@ -1,11 +1,12 @@
 package com.taskmanagement.task_management_system.Base;
 
 import com.taskmanagement.task_management_system.Enum.NotificationType;
+import com.taskmanagement.task_management_system.Model.dto.notification.NotificationRequest;
 import com.taskmanagement.task_management_system.Model.entity.Notification;
 import org.springframework.jmx.export.notification.UnableToSendNotificationException;
 
 public interface NotificationSender {
 
     NotificationType getType();
-    void send (Notification notification) throws UnableToSendNotificationException;
+    void send (NotificationRequest notification) throws UnableToSendNotificationException;
 }
