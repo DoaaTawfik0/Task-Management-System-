1009d293a830b64f74881b9800a9635fdfd31e10
Hasnaa <a02040456@gmail.com>
2026-05-17 17:31:59 +0300

    fix the mismatch between controller path variables and method parameters

--- src/main/java/com/taskmanagement/task_management_system/Controller/TeamController.java
+++ src/main/java/com/taskmanagement/task_management_system/Controller/TeamController.java
@@ -70,7 +70,7 @@ public class TeamController {
         return ResponseEntity.ok("User with id:" + userId + " is Added successfully...");
     }
 
-    @PostMapping("/{id}/leave")
+    @PostMapping("/{id}/leave/{userId}")
     public ResponseEntity<Void> leaveTeam(@PathVariable Long id, @PathVariable Long userId) {
 
         service.leaveTeam(id, userId);
